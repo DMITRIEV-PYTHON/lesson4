96acee31e98004193df5e7f5706a964026b71fbc
DMITRIEV-PYTHON <dmitriev.python@gmail.com>
2024-12-07 20:19:19 +0300

    file

---
+++ module2.py
@@ -0,0 +1,14 @@
+def say_hi():
+    print("Я из функции в модуле2")
+
+
+def main():
+    a = 5
+    b = 10
+    print("Привет")
+
+
+print(__name__)
+
+if __name__ == "__main__":
+    main()
---
+++ test_2.py
@@ -0,0 +1,7 @@
+from module2 import a
+
+print("Привет, я из модуля 1!")
+
+say_hi()
+print(a)
+print(b)
